6666f8b11128e3ee81bcf10818560f7a12d41ba4
Александр <Aleksandr@Aleksandrs-MacBook.local>
2015-05-26 22:16:06 +0300

    init

---
+++ SourceTreeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceTreeTest
+//
+//  Created by Александр on 26.05.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SourceTreeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SourceTreeTest
+//
+//  Created by Александр on 26.05.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
